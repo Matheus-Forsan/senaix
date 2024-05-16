577109abb9963711d86bccda51215e86a1a61ffa
matheus marques <matheusforsan@gmail.com>
2024-05-16 08:59:21 -0300

    perfil

--- src/context/useAuth.js
+++ src/context/useAuth.js
@@ -55,10 +55,13 @@ import React, {
       await AsyncStorage.multiRemove(["@SenaiX:token", "@SenaiX:user"]);
       setData({});
     }, []);
-  
+    const updateUser = useCallback((updateUser) => {
+      setData((currentData) => ({...currentData, user :updatedUser}));
+      AsyncStorage.setItem("@SenaiX:user", JSON.stringify(updatedUser));
+    }, []);
   
     return (
-      <AuthContext.Provider value={{ ...data, signIn, signOut }}>
+      <AuthContext.Provider value={{ ...data, signIn, signOut, updateUser }}>
         {children}
       </AuthContext.Provider>
     );
